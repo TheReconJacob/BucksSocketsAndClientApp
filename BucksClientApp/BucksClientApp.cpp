d16b2a70a46c52f4ada6c9ff9bc0d470e12ade13
thereconjacob <jvpokeyjake@gmail.com>
2023-03-03 16:21:59 +0000

    Do server and client exercises

---
+++ BucksClientApp/BucksClientApp.cpp
@@ -0,0 +1,60 @@
+#include <tchar.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include "iostream"
+
+int main()
+{
+    SOCKET clientSocket;
+    int port = 55555;
+    WSADATA wsaData;
+    int wsaerr;
+    WORD wVersionRequested = MAKEWORD(2, 2);
+    wsaerr = WSAStartup(wVersionRequested, &wsaData);
+    if (wsaerr != 0) {
+        std::cout << "The Winsock dll not found!" << std::endl;
+        return 0;
+    }
+    else {
+        std::cout << "The Winsock dll found!" << std::endl;
+        std::cout << "The status: " << wsaData.szSystemStatus << std::endl;
+    }
+
+    clientSocket = INVALID_SOCKET;
+    clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cout << "Error at socket(): " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 0;
+    }
+    else {
+        std::cout << "socket() is OK!" << std::endl;
+    }
+
+    sockaddr_in clientService;
+    clientService.sin_family = AF_INET;
+    InetPton(AF_INET, _T("127.0.0.1"), &clientService.sin_addr.s_addr);
+    clientService.sin_port = htons(port);
+    if (connect(clientSocket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR) {
+        std::cout << "Client: connect() - Failed to connect." << std::endl;
+        WSACleanup();
+        return 0;
+    }
+    else {
+        std::cout << "Client: connect() is OK." << std::endl;
+        std::cout << "Client: Can start sending and receiving data..." << std::endl;
+    }
+    system("pause");
+    WSACleanup();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
